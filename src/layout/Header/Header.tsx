2a628c4c9252fc97b8bfb7dde0277898220d9ee2
Dima <mistik1321@yandex.ru>
2025-04-04 22:47:54 +0300

    react_shop

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -8,20 +8,23 @@ import style from './style.module.scss'
 // const Header = ({title}: IProps) => {
 const Header = () => {
     return (
-        <header className={style.headFoot}>
+        <header className={`${style.headFoot} ${style.flex}`}>
             <div className={`${style.container} ${style.headFootWrapper}`}>
                 <h2 className={style.headLink}>
                     {/*<Link to='/'>{title}</Link>*/}
-                    <p>React Shop</p>
+                    <a href="/">React Shop</a>
                 </h2>
                 <h3>
                     <a href="#">Огромный выбор товаров, скинов по любимой игре на ваш вкус!</a>
                 </h3>
                 <h3 className={style.headLink}>
                     {/*<Link to='/'>Главная страница</Link>*/}
-                    <p>Главная страница</p>
+                    <a href='/'>Главная страница</a>
                 </h3>
             </div>
+            <h3 className={`${style.headFootWrapper} ${style.headLink}`}>
+                <a href='https://github.com/Dima11221/react_shop/tree/main' target='_blank'>Ссылка на репозиторий</a>
+            </h3>
         </header>
     )
 
