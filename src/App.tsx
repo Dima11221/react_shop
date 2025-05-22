9afa199f5ff56aac0dfc1b58494d79f98a5f59bb
Dima <mistik1321@yandex.ru>
2025-05-22 22:41:15 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import {Header} from "./layout/Header/Header.tsx";
 import {Footer} from "./layout/Footer/Footer.tsx";
 import {Shop} from "./components/Shop/Shop.tsx";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
 
 
     return (
-        <>
-            <>
-                {/*<Header title='React Shop' />*/}
-                <Header />
-                {/*<Footer text='GitHub page' />*/}
+        // <>
+        //     <>
+        //         {/*<Header title='React Shop' />*/}
+        //         <Header />
+        //         {/*<Footer text='GitHub page' />*/}
+        //
+        //         <Shop />
+        //         <Footer />
+        //     </>
+        // </>
 
-                <Shop />
-                <Footer />
-            </>
-        </>
+        <Router>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/react_shop" element={<Shop />}/>
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
     )
 }
 
