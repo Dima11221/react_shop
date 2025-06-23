55ce5fd985d6248f9e9e06c37105796e1c1fa592
Dima <mistik1321@yandex.ru>
2025-06-23 23:23:29 +0300

    добавлен сервер

--- src/components/Preloader/Preloader.tsx
+++
@@ -1,9 +0,0 @@
-import style from './style.module.scss'
-
-const Preloader = () => {
-    return (
-        <div className={style.preloader}></div>
-    )
-}
-
-export {Preloader}
--- src/config.ts
+++
@@ -1,8 +0,0 @@
-const API_KEY = import.meta.env.VITE_APP_API;
-
-const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru';
-
-export {
-    API_URL,
-    API_KEY
-}
