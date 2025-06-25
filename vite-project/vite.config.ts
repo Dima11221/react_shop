0c43657f416ccf126dbbef31abef142043d37307
Dima <mistik1321@yandex.ru>
2025-06-25 23:42:52 +0300

    исправление багов

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react_shop/',
+  base: '/fortnite_shop/',
   plugins: [
     react(),
     svgr(),
