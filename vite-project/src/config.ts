6222c593302393cd41e7220f089a9294a32588e3
Dima <mistik1321@yandex.ru>
2025-07-15 00:08:42 +0300

    настройка для деплоя сервера с товарами

--- vite-project/src/config.ts
+++ vite-project/src/config.ts
@@ -1,13 +1,15 @@
+// import * as process from "node:process";
+
 const API_KEY = import.meta.env.VITE_APP_API;
 
 const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru';
 const API_BACKEND_URL = 'https://react-shop-backend-672m.onrender.com';
 // const GOODS_WS_URL = import.meta.env.VITE_GOODS_WS_URL || 'ws://localhost:3002';
-// const GOODS_WS_URL = 'ws://localhost:3002';
+const GOODS_WS_URL = import.meta.env.WS_URL || 'ws://localhost:3002';
 
-const GOODS_WS_URL = import.meta.env.PROD
-  ? 'wss://your-render-app.onrender.com'
-  : 'ws://localhost:3002';
+// const GOODS_WS_URL = import.meta.env.PROD
+//   ? 'wss://your-render-app.onrender.com'
+//   : 'ws://localhost:3002';
 
 export {
     API_URL,
